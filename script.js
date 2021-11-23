6a05609e9df2b1db5fc4b250abf8bed400848103
blackeagle94 <blackeagle4894@gmail.com>
2021-11-23 21:00:52 +0100

    completed

--- script.js
+++ script.js
@@ -67,173 +67,149 @@ for(let j = 0; j < historySubTitles.length; j++) {
 
 /* PLAYERS SECTION this section was created using only DOM*/
 
-const playerInfos = [
-    {
-        name: 'Mert Günok', number: '34', position: 'Goalkeeper', imgUrl: 'https://images.bjk.com.tr/images/players/mert-gunok_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Ersin Destanoglu', number:'30', position: 'Goalkeeper', imgUrl: 'https://images.bjk.com.tr/images/players/ersin-destanoglu-2-2_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Emre Bilgin', number: '61', position: 'Goalkeeper', imgUrl: 'https://images.bjk.com.tr/images/players/emre-bilgin-2_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Ridvan Yilmaz', number: '3', position: 'Defender', imgUrl: 'https://images.bjk.com.tr/images/players/ridvan-yilmaz-2-2_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Serdar Saatci', number: '46', position: 'Defender', imgUrl: 'https://images.bjk.com.tr/images/players/serdar-saatci-2_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Domagoj Vida', number: '24', position: 'Defender', imgUrl: 'https://images.bjk.com.tr/images/players/domagoj-vida_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/hr.png'
-    },
-    {
-        name: 'Welinton Souza Silva', number: '23', position: 'Defender', imgUrl: 'https://images.bjk.com.tr/images/players/welinton-souza-silva_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/br.png'
-    },
-    {
-        name: 'Francisco Montero', number: '4', position: 'Defender', imgUrl: 'https://images.bjk.com.tr/images/players/francisco-montero_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/es.png'
-    },
-    {
-        name: 'Valentin Rosier', number: '2', position: 'Defender', imgUrl: 'https://images.bjk.com.tr/images/players/valentin-rosier_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/fr.png'
-    },
-    {
-        name: 'Umut Meras', number: '77', position: 'Defender', imgUrl: 'https://images.bjk.com.tr/images/players/umut-meras_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Necip Uysal', number: '20', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/necip-uysal_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Oguzhan Ozyakup', number: '10', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/oguzhan-ozyakup_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Atiba Hutchinson', number: '13', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/atiba-hutchinson_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/ca.png'
-    },
-    {
-        name: 'Gokhan Tore', number: '11', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/gokhan-tore_229x270_000002.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Georges-Kevin Nkoudou', number: '7', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/georges-kevin-nkoudou_229x270_000003.png', nationality:'https://bjk.com.tr/img/country-flags/png100px/fr.png'
-    },
-    {
-        name: 'Ajdin Hasic', number: '19', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/ajdin-hasic_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/ba.png'
-    },
-    {
-        name: 'Huseyin Atakan Uner', number: '27', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/huseyin-atakan-uner_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Josef de Souza', number: '5', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/josef-de-souza_229x270_000003.png', nationality:'https://bjk.com.tr/img/country-flags/png100px/br.png'
-    },
-    {
-        name: 'Rachid Ghezzal', number: '18', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/rachid-ghezzal_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/dz.png'
-    },
-    {
-        name: 'Salih Ucan', number: '8', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/salih-ucan_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Mehmet Topal', number: '14', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/mehmet-topal_229x270_000002.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Alex Teixeira', number: '90', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/alex-teixeira_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/br.png'
-    },
-    {
-        name: 'Can Bozdogan', number: '12', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/can-bozdogan_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Miralem Pjanic', number: '15', position: 'Midfielder', imgUrl: 'https://images.bjk.com.tr/images/players/miralem-pjanic_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/ba.png'
-    },
-    {
-        name: 'Cyle Christopher Larin', number: '17', position: 'Forward', imgUrl: 'https://images.bjk.com.tr/images/players/cyle-christopher-larin_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/ca.png'
-    },
-    {
-        name: 'Guven Yalcin', number: '50', position: 'Forward', imgUrl: 'https://images.bjk.com.tr/images/players/guven-yalcin_229x270.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Kenan Karaman', number: '28', position: 'Forward', imgUrl: 'https://images.bjk.com.tr/images/players/kenan-karaman_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/tr.png'
-    },
-    {
-        name: 'Michy Batshuayi', number: '9', position: 'Forward', imgUrl: 'https://images.bjk.com.tr/images/players/michy-batshuayi_229x270_000001.jpg', nationality:'https://bjk.com.tr/img/country-flags/png100px/be.png'
+//Player infos fetched from my own api
+fetch('https://blackeagleapi.herokuapp.com/api/besiktas-players').then(res => res.json()).then(playerInfos => {
+    const playersContainer = document.getElementById('players')
+    playersContainer.style.display = 'flex'
+    playersContainer.style.justifyContent = 'center'
+    playersContainer.style.flexWrap = 'wrap'
+    //section title
+    let h1 = document.createElement('h1')
+    let textH1 = document.createTextNode('PLAYERS')
+    h1.appendChild(textH1)
+    //style h1
+    h1.style.marginLeft = '50%' 
+    h1.style.marginRight = '50%'
+    playersContainer.appendChild(h1)
+
+    for(let i = 0; i <playerInfos.length; i++) {
+        //new Div for each player
+        let newDiv = document.createElement('div')
+        //created img tag for player
+        let img = document.createElement('img')
+        img.src = playerInfos[i].imgUrl
+        
+        // created text node and appended p tag for player name
+        let p = document.createElement('p')
+        let name = document.createTextNode(playerInfos[i].name)
+        p.appendChild(name)
+    
+        //created text node and appended p tag for player number
+        let numberP = document.createElement('p')
+        let number = document.createTextNode(playerInfos[i].number)
+        numberP.appendChild(number)
+
+        //created img tag for national flag
+        let imgFlag = document.createElement('img')
+        imgFlag.src = playerInfos[i].nationality
+
+        //created text node and appended p tag for player position
+        let positionP = document.createElement('p')
+        let position = document.createTextNode(playerInfos[i].position)
+        positionP.appendChild(position)
+
+
+        /* STYLES */
+        //newDiv
+        newDiv.style.margin = '5px'
+
+        //img player
+    
+
+        //img flag
+        imgFlag.style.float = 'right'
+        imgFlag.style.marginTop = '-43px'
+        imgFlag.style.width = '20%'
+        imgFlag.style.zIndex = '1'
+
+        //number
+        numberP.style.backgroundColor = 'black'
+        numberP.style.color = 'white'
+        numberP.style.width = '20%'
+        numberP.style.padding = '5px'
+        numberP.style.marginTop = '-3px'
+        numberP.style.marginLeft = '5px'
+        numberP.style.zIndex = '1'
+        numberP.style.textAlign = 'center'
+
+        //position
+        positionP.style.marginTop = '-40px'
+        positionP.style.marginLeft = '75px'
+        positionP.style.fontStyle = 'italic'
+
+        //player name
+        p.style.textAlign = 'start'
+        p.style.fontWeight = 'bold'
+
+
+
+        //appended all components to newDiv
+        newDiv.appendChild(img)
+        newDiv.appendChild(numberP)
+        newDiv.appendChild(imgFlag)
+        newDiv.appendChild(positionP)
+        newDiv.appendChild(p)
+        
+        playersContainer.appendChild(newDiv)
     }
-]
-
-const playersContainer = document.getElementById('players')
-playersContainer.style.display = 'flex'
-playersContainer.style.justifyContent = 'center'
-playersContainer.style.flexWrap = 'wrap'
-//section title
-let h1 = document.createElement('h1')
-let textH1 = document.createTextNode('PLAYERS')
-h1.appendChild(textH1)
-//style h1
-h1.style.marginLeft = '50%' 
-h1.style.marginRight = '50%'
-playersContainer.appendChild(h1)
-
-for(let i = 0; i <playerInfos.length; i++) {
-    //new Div for each player
-    let newDiv = document.createElement('div')
-    //created img tag for player
-    let img = document.createElement('img')
-    img.src = playerInfos[i].imgUrl
-    
-    // created text node and appended p tag for player name
-    let p = document.createElement('p')
-    let name = document.createTextNode(playerInfos[i].name)
-    p.appendChild(name)
-   
-    //created text node and appended p tag for player number
-    let numberP = document.createElement('p')
-    let number = document.createTextNode(playerInfos[i].number)
-    numberP.appendChild(number)
-
-    //created img tag for national flag
-    let imgFlag = document.createElement('img')
-    imgFlag.src = playerInfos[i].nationality
-
-    //created text node and appended p tag for player position
-    let positionP = document.createElement('p')
-    let position = document.createTextNode(playerInfos[i].position)
-    positionP.appendChild(position)
-
-
-    /* STYLES */
-    //newDiv
-    newDiv.style.margin = '5px'
-
-    //img player
-  
-
-    //img flag
-    imgFlag.style.float = 'right'
-    imgFlag.style.marginTop = '-43px'
-    imgFlag.style.width = '20%'
-    imgFlag.style.zIndex = '1'
-
-    //number
-    numberP.style.backgroundColor = 'black'
-    numberP.style.color = 'white'
-    numberP.style.width = '20%'
-    numberP.style.padding = '5px'
-    numberP.style.marginTop = '-3px'
-    numberP.style.marginLeft = '5px'
-    numberP.style.zIndex = '1'
-    numberP.style.textAlign = 'center'
-
-    //position
-    positionP.style.marginTop = '-40px'
-    positionP.style.marginLeft = '75px'
-    positionP.style.fontStyle = 'italic'
-
-    //player name
-    p.style.textAlign = 'start'
-    p.style.fontWeight = 'bold'
+}).catch(err => console.log(err))
 
 
 
-    //appended all components to newDiv
-    newDiv.appendChild(img)
-    newDiv.appendChild(numberP)
-    newDiv.appendChild(imgFlag)
-    newDiv.appendChild(positionP)
-    newDiv.appendChild(p)
-    
-    playersContainer.appendChild(newDiv)
+/*Vodafone Park SECTION*/
+
+//hide all contents except first
+const stadiumContentItems = document.getElementsByClassName('stadium-content-item')
+for(let i = 1; i < stadiumContentItems.length; i++) {
+    stadiumContentItems[i].style.display = 'none'
+}
+
+//handling on click function of stadium subtitles
+const stadiumSubTitles = document.getElementsByClassName('stadium-listItem')
+for(let j = 0; j < stadiumSubTitles.length; j++) {
+    stadiumSubTitles[j].addEventListener('click', () => {
+        for(let i = 0; i < stadiumContentItems.length; i++) {
+            stadiumContentItems[i].style.display = 'none'
+        }
+
+        stadiumContentItems[j].style.display = 'flex'
+    })
+}
+
+/* LOGO */
+//goes top of the page when on click to logo
+const logo = document.querySelector('.logo')
+logo.addEventListener('click', () => window.scroll(0, 0))
+
+
+/* MODAL */
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+let iframe = document.getElementById('iframe')
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  iframe.src = iframe.src
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    iframe.src = iframe.src
+  }
 }
